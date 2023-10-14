166f937643a5ef2359b6151b35245fc31e22af76
João Carllos <jocasilvalima@gmail.com>
2023-10-13 21:23:19 -0300

    Spring Boot project

---
+++ src/main/java/com/joaocarlos/aulasimple/AulasimpleApplication.java
@@ -0,0 +1,13 @@
+package com.joaocarlos.aulasimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulasimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulasimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaocarlos/aulasimple/AulasimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaocarlos.aulasimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulasimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
